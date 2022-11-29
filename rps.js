0a50741717a59dc685e7971dee35d1bceefb97bd
jimram85 <jimram85@gmail.com>
2022-11-29 14:12:41 -0800

    added rock paper scissor game frame, added play round function and prompt

--- rps.js
+++ rps.js
@@ -1,7 +1,30 @@
-function getComputerChoice(rps){
-    return rps [Math.floor(Math.random() * rps.length)];
+//tools
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+//player entry
+let enterRockPaperScissors = prompt ("Enter Rock, Paper, or Scissors...");
+
+//pick rock paper scissors randomly (computer)
+function getComputerChoice(pickRandomTool){
+    return pickRandomTool [Math.floor(Math.random() * pickRandomTool.length)];
    
 }
-const pick = getComputerChoice (['Rock', 'Paper', 'Scissors']);
+ //plays round for rock paper scissors 
+   function playRound(playerSelection, computerSelection){
+    if (playerSelection === computerSelection){
+        return 'Its a draw.';
+    } else if ((playerSelection === rock && computerSelection === paper) ||
+        (playerSelection === paper && computerSelection === scissors) || 
+        (playerSelection === scissors && computerSelection === rock)) {
+        return 'you lost this one.';
+    } else 
+        return "Wow, you got one..."
+}
 
-    console.log(pick)
+//choice of tool
+const computerSelection = getComputerChoice ([rock, paper, scissors]);
+const playerSelection = enterRockPaperScissors;
+
+ console.log(playRound(playerSelection, computerSelection));
